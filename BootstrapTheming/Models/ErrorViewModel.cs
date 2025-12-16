f9ef9f51b18b62f9a6461610bfc38a96fbd1240b
= <kevinmorgan@hotmail.com>
2025-12-16 09:25:07 -0600

    Add project files.

---
+++ BootstrapTheming/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootstrapTheming.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
